ed7f711fd849cb78d58d9badf35744f760ee9cfa
chay140 <chaihyunpark140@gmail.com>
2025-08-04 00:52:35 +0900

    [FE] add: added product card component

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,12 +1,59 @@
-import type { ProductType } from "@/types/ProductType"
+import type { ProductType } from "@/types/ProductType";
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { PencilSimpleIcon, TrashIcon } from "@phosphor-icons/react";
+import { useColorModeValue } from "./ui/color-mode";
 
 type ProductCardProps = {
-  product: ProductType
-}
-const ProductCard = ({product}: ProductCardProps) => {
-  return (
-    <div>ProductCard</div>
-  )
-}
+  product: ProductType;
+};
+const ProductCard = ({ product }: ProductCardProps) => {
+  const textColor = useColorModeValue("gray.600", "gray.200");
+  const backgroundColor = useColorModeValue("white", "gray.800");
 
-export default ProductCard
+  return (
+    <Box
+      shadow="lg"
+      rounded="lg"
+      overflow="hidden"
+      transition="all 0.3s"
+      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+      bg={backgroundColor}
+    >
+      <Image
+        src={product.image}
+        alt={product.name}
+        h={48}
+        w="full"
+        objectFit="cover"
+      />
+
+      <Box p={4}>
+        <Heading as="h3" size="md" mb={2}>
+          {product.name}
+        </Heading>
+
+        <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack gap={2}>
+          <IconButton colorScheme="blue">
+            <PencilSimpleIcon />
+          </IconButton>
+          <IconButton colorScheme="red">
+            <TrashIcon />
+          </IconButton>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductCard;
